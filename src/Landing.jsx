693c00bc0077108342dba54137a24eaa718c6fd4
Albar Malik <albar2806@student.abudzar.sch.id>
2026-05-03 12:45:37 +0700

    Initialize 'landing': Hero section

---
+++ src/Landing.jsx
@@ -0,0 +1,18 @@
+import { ArrowRight } from 'lucide-react';
+import { useState } from 'react'
+
+function Landing() {
+  return (
+    <>
+      <section>
+        <div className=""></div>
+        <div className="">
+          <button>Pelajari Lebih Lanjut</button>
+          <button>Mulai Sekarang {<ArrowRight />}</button>
+        </div>
+      </section>
+    </>
+  )
+}
+
+export default Landing;
