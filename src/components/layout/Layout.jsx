53d603c250e3777f3e0ad37fa0a6929affebcf34
raffa <raffahitipeuw@gmail.com>
2026-05-10 17:33:16 +0700

    lenis

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,11 +1,32 @@
 import { Outlet } from "react-router";
+import { useEffect } from "react";
+import Lenis from "lenis";
 
 export default function Layout() {
-    return(
-        <>
-            <main>
-                <Outlet/>
-            </main>
-        </>
-    )
+
+    useEffect(() => {
+        const lenis = new Lenis({
+            duration: 1.2,
+            smoothWheel: true,
+            smoothTouch: true,
+            touchMultiplier: 2,
+        });
+
+        function raf(time) {
+            lenis.raf(time);
+            requestAnimationFrame(raf);
+        }
+
+        requestAnimationFrame(raf);
+
+        return () => {
+            lenis.destroy();
+        };
+    }, []);
+
+    return (
+        <main>
+            <Outlet />
+        </main>
+    );
 }
