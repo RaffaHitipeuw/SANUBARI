8a47eddf7b62060295211715ef57e5348b86a871
raffa <raffahitipeuw@gmail.com>
2026-05-17 19:23:11 +0700

    asd

--- src/components/layout/LayoutDashboard.jsx
+++ src/components/layout/LayoutDashboard.jsx
@@ -2,49 +2,29 @@ import React, { useState, useEffect } from 'react';
 import { onAuthStateChanged } from "firebase/auth";
 import { auth } from "../../../firebase-config";
 import { Outlet, NavLink, useLocation } from 'react-router';
-import { Home, Camera, ChartColumnBig, Newspaper, BotMessageSquare, LogOut, Info, CircleQuestionMark, Paperclip, Ellipsis, X } from 'lucide-react';
+import {
+  Home,
+  Camera,
+  ChartColumnBig,
+  Newspaper,
+  BotMessageSquare,
+  LogOut,
+  Info,
+  CircleQuestionMark,
+  Paperclip,
+  Ellipsis,
+  X
+} from 'lucide-react';
 import { Logo } from '../sections/Assets';
 
 export default function LayoutDashboard() {
   const [userName, setUserName] = useState("");
   const [isExpanded, setIsExpanded] = useState(false);
   const [showAbout, setShowAbout] = useState(false);
-  const [extraMenu, setExtraMenu] = useState(false);
-  const [preview, setPreview] = useState(null);
-  const [selectedFile, setSelectedFile] = useState(null);
-  const [message, setMessage] = useState("");
-  const [loading, setLoading] = useState(false);
-
-  const handleSendSupport = async () => {
-    if (!message.trim() && !preview) return;
-
-    try {
-      setLoading(true);
-      const formData = new FormData();
-      formData.append("message", message);
-
-      if (preview) {
-        const response = await fetch(preview);
-        const blob = await response.blob();
-        formData.append("image", blob, "screenshot.png");
-      }
-
-      await fetch("http://localhost:3000/support", {
-        method: "POST",
-        body: formData
-      });
-
-      setMessage("");
-      setPreview(null);
-      setSupportOpen(false);
-    } catch (error) {
-      console.log(error);
-    } finally {
-      setLoading(false);
-    }
-  };
+  const [extraMenu, setExtraMenu] = useState(false)
 
   const location = useLocation();
+
   const [open, setOpen] = useState(false);
   const [infoOpen, setInfoOpen] = useState(false);
   const [menuHover, setMenuHover] = useState(false);
@@ -68,20 +48,21 @@ export default function LayoutDashboard() {
   }, []);
 
   return (
-    <div className='flex max-sm:flex-col gap-4 min-h-screen bg-sariwhite relative'>
+    <div className='flex max-sm:flex-col gap-4 h-screen overflow-hidden bg-sariwhite relative'>
       <div className="hidden max-sm:absolute max-sm:w-full z-49 p-4 max-sm:flex flex-col gap-2 items-end">
         <button className='dsh-cards border-sariblack/14 cursor-pointer bg-white' onClick={() => setExtraMenu(!extraMenu)}>
           <Ellipsis className={`size-6 ${extraMenu ? 'hidden' : 'block'}`}/>
           <X className={`size-6 ${extraMenu ? 'block' : 'hidden'}`}/>
         </button>
-
         <div className={`${extraMenu ? 'flex' : 'hidden'} flex-col p-2 items-end w-[60vw] bg-white border border-sariblack/14 rounded-2xl overflow-hidden`}>
           <ul className={`${extraMenu ? 'max-sm:flex' : 'max-sm:hidden'} flex-col hidden w-full`}>
             <li onClick={() => setInfoOpen(!infoOpen)} className={`${infoOpen ? 'h-max flex-col py-4 items-stretch gap-2' : 'w-full items-center'} text-left flex h-14 px-6 text-base hover:bg-sariblack/8 transition duration-75 w-full cursor-pointer`}>
               {infoOpen == false && (
                 <>
                   <Info size={18} />
-                  <span className='ml-2 whitespace-nowrap transition-all duration-500'>About</span>
+                  <span className={`ml-2 whitespace-nowrap transition-all duration-500`}>
+                    About
+                  </span>
                 </>
               )}
               {infoOpen && (
@@ -96,18 +77,22 @@ export default function LayoutDashboard() {
                 </>
               )}
             </li>
-
             <li>
               <button onClick={() => setSupportOpen(true)} className='flex items-center h-14 px-6 text-base hover:bg-sariblack/8 transition duration-75 w-full cursor-pointer'>
                 <CircleQuestionMark size={18} />
-                <span className='ml-2 whitespace-nowrap transition-all duration-500'>Support</span>
+                <span className={`ml-2 whitespace-nowrap transition-all duration-500`}>
+                  Support
+                </span>
               </button>
             </li>
-
             <li>
-              <NavLink className='bg-sarired/14 text-sarired flex items-center h-14 px-6 text-base rounded-xl'>
+              <NavLink
+                className='bg-sarired/14 text-sarired flex items-center h-14 px-6 text-base rounded-xl'
+              >
                 <LogOut size={18} />
-                <span className='ml-2 whitespace-nowrap transition-all duration-500'>Log Out</span>
+                <span className={`ml-2 whitespace-nowrap transition-all duration-500`}>
+                  Log Out
+                </span>
               </NavLink>
             </li>
           </ul>
@@ -115,8 +100,11 @@ export default function LayoutDashboard() {
       </div>
 
       <div className="pr-0 max-sm:pr-4 p-4 h-full max-sm:h-max max-sm:w-full max-sm:absolute max-sm:bottom-0 max-sm:z-999">
-        <header className={`${open ? 'w-[20vw] max-sm:w-full' : 'w-max max-sm:w-full'} flex flex-col max-sm:flex-row gap-4 justify-between h-full transition-[width] duration-1000 ease-[cubic-bezier(0.3,1,0.2,1)]`} onMouseEnter={() => setOpen(true)} onMouseLeave={() => { setOpen(false); setInfoOpen(false); }}>
-          <nav className="relative bg-white overflow-hidden border border-sariblack/14 h-full max-sm:h-max max-sm:w-full rounded-2xl flex flex-col max-sm:flex-row items-stretch">
+        <header className={`${open ? 'w-[20vw] max-sm:w-full' : 'w-max max-sm:w-full'} flex flex-col max-sm:flex-row gap-4 justify-between h-full transition-[width] duration-1000 ease-[cubic-bezier(0.3,1,0.2,1)]`} onMouseEnter={() => setOpen(true)} onMouseLeave={() => {
+          setOpen(false);
+          setInfoOpen(false);
+        }}>
+          <nav className="relative bg-white overflow-clip border border-sariblack/14 h-full max-sm:h-max max-sm:w-full rounded-2xl flex flex-col max-sm:flex-row items-stretch justify-between">
             <NavLink to={'/profile'} className='flex items-center p-4 max-sm:hidden'>
               <span className='block w-12 h-12 p-2 border border-sariblack/14 rounded-full bg-white'>
                 <Logo className={'text-sarired'} />
@@ -125,8 +113,10 @@ export default function LayoutDashboard() {
                 {userName}
               </h2>
             </NavLink>
-
-            <ul className={`flex flex-col max-sm:flex-row gap-2 max-sm:gap-1 p-4 max-sm:p-2 max-sm:justify-between max-sm:w-full overflow-y-auto max-sm:overflow-auto transition-all duration-500 max-sm:h-16 ${infoOpen ? 'h-[32vh]' : 'h-full'}`} style={{ scrollbarWidth: 'none' }}>
+            <ul
+              className='flex flex-col max-sm:flex-row h-full gap-2 max-sm:gap-1 p-4 max-sm:p-2 max-sm:justify-between max-sm:w-full max-sm:overflow-auto max-sm:h-16'
+              style={{ scrollbarWidth: 'none' }}
+            >
               {navItems.map((item, index) => (
                 <li key={index}>
                   <NavLink to={item.path} className='border border-transparent hover:border-sarired hover:bg-sarired/14 rounded-2xl max-sm:rounded-xl hover:text-sarired transition duration-75 flex items-center max-sm:justify-center px-4 max-sm:p-0 h-14 max-sm:h-full max-sm:w-auto text-base max-sm:aspect-square max-sm:size-14'>
@@ -138,28 +128,43 @@ export default function LayoutDashboard() {
                 </li>
               ))}
             </ul>
-
             <ul className='flex flex-col border-t border-sariblack/14 max-sm:hidden'>
               <li>
-                <button onClick={() => setSupportOpen(true)} className='flex items-center h-14 px-8 text-base hover:bg-sariblack/8 transition duration-75 w-full cursor-pointer'>
+                <button
+                  onClick={() => setSupportOpen(true)}
+                  className='flex items-center h-14 px-8 text-base hover:bg-sariblack/8 transition duration-75 w-full cursor-pointer'
+                >
                   <CircleQuestionMark size={18} />
-                  <span className={`${open ? 'ml-2 w-5' : 'w-0 opacity-0'} whitespace-nowrap transition-all duration-500`}>Support</span>
+                  <span
+                    className={`${open ? 'ml-2 w-5' : 'w-0 opacity-0'} whitespace-nowrap transition-all duration-500`}
+                  >
+                    Support
+                  </span>
                 </button>
               </li>
               <li>
-                <NavLink className='bg-sarired/14 text-sarired flex items-center h-14 px-8 text-base'>
+                <NavLink
+                  className='bg-sarired/14 text-sarired flex items-center h-14 px-8 text-base'
+                >
                   <LogOut size={18} />
-                  <span className={`${open ? 'ml-2 w-5' : 'w-0 opacity-0'} whitespace-nowrap transition-all duration-500`}>Log Out</span>
+                  <span className={`${open ? 'ml-2 w-5' : 'w-0 opacity-0'} whitespace-nowrap transition-all duration-500`}>
+                    Log Out
+                  </span>
                 </NavLink>
               </li>
             </ul>
           </nav>
-
-          <button onClick={() => { open ? setInfoOpen(!infoOpen) : false }} className={`${infoOpen ? 'w-full h-max' : 'w-12'} aspect-square flex flex-col items-center justify-center rounded-2xl border border-sariblack/14 text-sariblack/40 cursor-pointer max-sm:hidden`}>
+          <button
+            onClick={() => {
+              open ? setInfoOpen(!infoOpen) : false
+            }}
+            className={`${infoOpen ? 'w-full h-max' : 'w-12'} aspect-square flex flex-col items-center justify-center rounded-2xl border border-sariblack/14 text-sariblack/40 cursor-pointer max-sm:hidden`}
+          >
             {infoOpen == false && <Info size={20} />}
             {open && infoOpen && (
               <>
                 <p>SANUBARI</p>
+
                 <p>
                   © 2026 SANUBARI. Medical Disclaimer:
                   For informational purposes only.
@@ -176,51 +181,44 @@ export default function LayoutDashboard() {
         <Outlet />
       </main>
 
-      {supportOpen && (
-        <div onClick={() => setSupportOpen(false)} className='fixed inset-0 max-sm:pb-20 bg-black/40 backdrop-blur-sm z-50 flex items-center justify-center px-6'>
-          <div onClick={(e) => e.stopPropagation()} className='w-[40vw] max-sm:w-full h-[73vh] max-sm:h-[80vh] bg-white dsh-cards px-7 py-6 max-sm:px-5 max-sm:py-5 shadow-2xl flex flex-col overflow-auto gap-6 max-sm:gap-4 max-h-max' style={{scrollbarWidth: 'none'}}>
-            <div className='flex flex-col leading-none shrink-0'>
-              <h1 className='text-4xl max-sm:text-2xl font-semibold font-mr text-sariblack'>
-                Butuh Bantuan? <br/>
-                <span className='text-sariblue'>Hubungi Kami!</span>
-              </h1>
-            </div>
-
-            <div className='flex flex-col'>
-              <div className='w-full h-58 max-sm:h-45 rounded-3xl max-sm:rounded-xl border border-sariblack/10 bg-sariwhite p-4 overflow-auto flex flex-col gap-3'>
-                {preview && (
-                  <div className='w-24 rounded-2xl border border-sariblack/10 bg-white p-2 shrink-0'>
-                    <img src={preview} alt='preview' className='w-full h-24 object-cover rounded-xl' />
-                    <p className='text-[10px] text-sariblack/50 mt-2 break-words leading-tight'>Screenshot yang dilampirkan</p>
-                  </div>
-                )}
-                <textarea value={message} onChange={(e) => setMessage(e.target.value)} placeholder='Pesan/Keluhan/Kritik...' className='w-full flex-1 resize-none bg-transparent outline-none text-sm placeholder:text-sariblack/25' />
+      {
+        supportOpen && (
+          <div onClick={() => setSupportOpen(false)} className='fixed inset-0 max-sm:pb-20 bg-black/40 backdrop-blur-sm z-50 flex items-center justify-center px-6'>
+            <div onClick={(e) => e.stopPropagation()} className='w-[40vw] max-sm:w-full h-[70vh] max-sm:h-[80vh] bg-white dsh-cards px-7 py-6 max-sm:px-5 max-sm:py-5 shadow-2xl flex flex-col overflow-auto gap-6 max-sm:gap-4 max-h-max' style={{scrollbarWidth: 'none'}}>
+              <div className='flex flex-col leading-none shrink-0'>
+                <h1 className='text-4xl max-sm:text-2xl font-semibold font-mr text-sariblack'>
+                  Butuh Bantuan? <br/>
+                  <span className='text-sariblue'>Hubungi Kami!</span>
+                </h1>
               </div>
-
-              <div className='mt-4 flex items-center gap-3 shrink-0'>
-                <label className='flex items-center rounded-2xl max-sm:rounded-xl border border-sariblue overflow-hidden transition cursor-pointer group max-sm:w-full'>
-                  <input type='file' accept='image/*' className='hidden' onChange={(e) => { const file = e.target.files[0]; if (file) { setSelectedFile(file); setPreview(URL.createObjectURL(file)); } }} />
-                  <div className='bg-sariblue self-stretch h-12 aspect-square flex items-center justify-center text-white shrink-0'>
-                    <Paperclip size={18} />
-                  </div>
-                  <div className='flex items-center gap-2 py-3 max-sm:py-2 px-6 max-sm:px-4 rounded-2xl max-sm:rounded-lg bg-sariwhite/60 font-semibold text-sariblue max-sm:text-sm z-2'>Lampirkan Screenshot</div>
-                </label>
-              </div>
-
-              <p className='text-xs text-sariblack/30 mt-3 leading-relaxed shrink-0'>Pesanmu akan terkirim lewat email yang terdaftar dalam SANUBARI.</p>
-
-              <div className='flex items-center max-sm:flex-col gap-3 mt-5 shrink-0'>
-                <button onClick={async () => { try { const formData = new FormData(); formData.append("message", message); if (selectedFile) { formData.append("image", selectedFile); } const response = await fetch("http://localhost:3000/support", { method: "POST", body: formData }); const data = await response.json(); if (data.success) { alert("Pesan berhasil dikirim"); setMessage(""); setPreview(null); setSelectedFile(null); } else { alert("Gagal mengirim pesan"); } } catch (error) { console.log(error); alert("Server error"); } }} className='flex items-center justify-center gap-2 py-3 max-sm:py-2 px-6 max-sm:px-4 rounded-2xl max-sm:rounded-lg bg-sariblue font-semibold text-white w-full max-sm:text-sm z-2 cursor-pointer'>
-                  Kirim
-                </button>
-                <button onClick={() => setSupportOpen(false)} className='flex items-center justify-center gap-2 py-3 max-sm:py-2 px-6 max-sm:px-4 rounded-2xl max-sm:rounded-lg bg-sarired/14 font-semibold text-sarired border border-sarired w-full max-sm:text-sm z-2 cursor-pointer'>
-                  Batal
-                </button>
+              <div className='flex flex-col'>
+                <textarea placeholder='Pesan/Keluhan/Kritik...' className='w-full max-sm:h-45 resize-none rounded-3xl max-sm:rounded-xl border border-sariblack/10 bg-sariwhite p-4 outline-none text-sm placeholder:text-sariblack/25'/>
+                <div className='mt-4 flex items-center gap-3 shrink-0'>
+                  <button className='flex items-center rounded-2xl max-sm:rounded-xl border border-sariblue overflow-hidden transition cursor-pointer group max-sm:w-full'>
+                    <div className='bg-sariblue self-stretch h-12 aspect-square flex items-center justify-center text-white shrink-0'>
+                      <Paperclip size={18} />
+                    </div>
+                    <div className='flex items-center gap-2 py-3 max-sm:py-2 px-6 max-sm:px-4 rounded-2xl max-sm:rounded-lg bg-sariwhite/60 font-semibold text-sariblue max-sm:text-sm z-2'>
+                      Lampirkan Screenshot
+                    </div>
+                  </button>
+                </div>
+                <p className='text-xs text-sariblack/30 mt-3 leading-relaxed shrink-0'>
+                  Pesanmu akan terkirim lewat email yang terdaftar dalam SANUBARI.
+                </p>
+                <div className='flex items-center max-sm:flex-col gap-3 mt-5 shrink-0'>
+                  <button className='flex items-center justify-center gap-2 py-3 max-sm:py-2 px-6 max-sm:px-4 rounded-2xl max-sm:rounded-lg bg-sariblue font-semibold text-white w-full max-sm:text-sm z-2 cursor-pointer'>
+                    Kirim
+                  </button>
+                  <button onClick={() => setSupportOpen(false)} className='flex items-center justify-center gap-2 py-3 max-sm:py-2 px-6 max-sm:px-4 rounded-2xl max-sm:rounded-lg bg-sarired/14 font-semibold text-sarired border border-sarired w-full max-sm:text-sm z-2 cursor-pointer'>
+                    Batal
+                  </button>
+                </div>
               </div>
             </div>
           </div>
-        </div>
-      )}
+        )
+      }
     </div>
   );
 }
