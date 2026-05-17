1effaee1ed1eed1cae3fdc9413eb26bceb347eb2
Albar Malik <albar2806@student.abudzar.sch.id>
2026-05-17 23:30:20 +0700

    OUMAYGAT SI RAFAAAA

--- firebase-config.js
+++ firebase-config.js
@@ -3,7 +3,7 @@ import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 
 const firebaseConfig = {
   apiKey: "AIzaSyDbMXAtkjQi6kzyfonEwo1MKH6XCMKjbuI",
-  authDomain: "sanubari-techsprint.firebaseapp.com",
+  authDomain: "://firebaseapp.com",
   projectId: "sanubari-techsprint",
   storageBucket: "sanubari-techsprint.firebasestorage.app",
   messagingSenderId: "165240553784",
