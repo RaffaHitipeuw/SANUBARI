0b2db1483df51f48b616344ce984f4179730b480
Albar Malik <albar2806@student.abudzar.sch.id>
2026-05-17 22:41:28 +0700

    Testimonial RSPONSV

--- src/components/sections/Testimonial.jsx
+++ src/components/sections/Testimonial.jsx
@@ -6,18 +6,21 @@ export default function Testimonial() {
     const testimonials = [
         {
             name: "Sam Nook",
-            role: "CEO at PT. Pujasera",
-            text: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sequi explicabo quo saepe vel. Iure laborum autem quam, similique, esse optio vero velit quia blanditiis, a vel obcaecati quos sunt laboriosam?",
-        },
-        {
-            name: "Raffa Hitipeuw",
-            role: "Frontend Developer",
-            text: "SANUBARI membantu saya memahami kondisi jantung dengan UI yang modern dan pengalaman yang nyaman.",
+            role: "Mahasiswa",
+            text: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sequi explicabo quo saepe vel. Iure.",
+            image: "/src/assets/images/testimonial-user.png"
         },
         {
             name: "Ahmad Fauzi",
-            role: "Mahasiswa",
-            text: "Fitur AI dan monitoring BPM real-time sangat membantu saya memahami kesehatan tubuh saya.",
+            role: "Dokter",
+            text: "Fitur AI dan monitoring BPM real-time sangat membantu saya memahami kesehatan tubuh.",
+            image: "/src/assets/images/testimonial-user2.png"
+        },
+        {
+            name: "Raindra Yunira",
+            role: "Frontend Developer",
+            text: "SANUBARI membantu saya memahami kondisi jantung dengan UI yang modern dan pengalaman yang nyaman.",
+            image: "/src/assets/images/testimonial-user3.png"
         },
     ];
 
@@ -71,133 +74,102 @@ export default function Testimonial() {
                 <h1 className="heading-1">Kata Mereka</h1>
             </div>
 
-            <div className="relative flex items-center justify-center min-h-[520px] w-full">
-                <button
-                    onClick={prevCard}
-                    className="navigation z-[999] absolute left-87"
-                    disabled={animating}
-                >
-                    <ArrowLeft size={18} />
-                </button>
+            <div className="relative flex items-center justify-center min-h-130 w-full px-4">
+                <div className="relative w-180 min-h-130 flex items-center justify-center">
+                    <button
+                        onClick={prevCard}
+                        className="navigation z-[999] absolute left-0 -translate-x-1/2 max-sm:translate-x-0 max-sm:bottom-0"
+                        disabled={animating}
+                    >
+                        <ArrowLeft size={18} />
+                    </button>
 
-                <button
-                    onClick={nextCard}
-                    className="navigation z-[999] absolute right-87"
-                    disabled={animating}
-                >
-                    <ArrowRight size={18} />
-                </button>
-
-                {testimonials.map((item, index) => {
-                    const isFront = cards[0] === index;
-                    const isMiddle = cards[1] === index;
-                    const isBack = cards[2] === index;
-
-                    if (!isFront && !isMiddle && !isBack) return null;
-
-                    const isMoving = movingCard === index;
-
-                    let bgColor = "";
-                    let zIndex = "";
-                    let transform = "";
-
-                    if (isFront) {
-                        bgColor = "bg-sariredlight";
-                        zIndex = "z-30";
-
-                        if (isMoving && direction === "next") {
-                            transform =
-                                "translate3d(5rem,-7rem,0) rotate(-7deg) scale(.88)";
-                        } else {
-                            transform =
-                                "translate3d(0rem,0rem,0) rotate(0deg) scale(1)";
+                    <button
+                        onClick={nextCard}
+                        className="navigation z-[999] absolute right-0 translate-x-1/2 max-sm:translate-x-0 max-sm:bottom-0"
+                        disabled={animating}
+                    >
+                        <ArrowRight size={18} />
+                    </button>
+                    {testimonials.map((item, index) => {
+                        const isFront = cards[0] === index;
+                        const isMiddle = cards[1] === index;
+                        const isBack = cards[2] === index;
+                        if (!isFront && !isMiddle && !isBack) return null;
+                        const isMoving = movingCard === index;
+                        let bgColor = "";
+                        let zIndex = "";
+                        let transform = "";
+                        if (isFront) {
+                            bgColor = "bg-sariredlight";
+                            zIndex = "z-30";
+                            if (isMoving && direction === "next") {
+                                transform =
+                                    "translate3d(5rem,-7rem,0) rotate(-7deg) scale(.88)";
+                            } else {
+                                transform =
+                                    "translate3d(0rem,0rem,0) rotate(0deg) scale(1)";
+                            }
                         }
-                    }
-                    else if (isMiddle) {
-                        bgColor = "bg-sarireddark";
-                        zIndex = "z-20";
-
-                        transform =
-                            "translate3d(0.8rem,1.5rem,0) rotate(2deg) scale(.96)";
-                        if (animating && direction === "next") {
+                        else if (isMiddle) {
+                            bgColor = "bg-sarireddark";
+                            zIndex = "z-20";
                             transform =
-                                "translate3d(-1rem,3rem,0) rotate(2deg) scale(.96)";
+                                "translate3d(0.8rem,1.5rem,0) rotate(2deg) scale(.96)";
+                            if (animating && direction === "next") {
+                                transform =
+                                    "translate3d(-1rem,3rem,0) rotate(2deg) scale(.96)";
+                            }
+                            if (animating && direction === "prev") {
+                                transform =
+                                    "translate3d(-2rem,3.6rem,0) rotate(3deg) scale(.94)";
+                            }
                         }
-                        if (animating && direction === "prev") {
+                        else if (isBack) {
+                            bgColor = "bg-sarireddarker";
+                            zIndex = "z-10";
                             transform =
-                                "translate3d(-2rem,3.6rem,0) rotate(3deg) scale(.94)";
+                                "translate3d(-1.8rem,3rem,0) rotate(4deg) scale(.92)";
+                            if (isMoving && direction === "prev") {
+                                zIndex = "z-40";
+                                transform =
+                                    "translate3d(5rem,-7rem,0) rotate(-7deg) scale(.88)";
+                            }
+                            else if (animating && direction === "next") {
+                                transform =
+                                    "translate3d(-2.5rem,4.6rem,0) rotate(5deg) scale(.9)";
+                            }
                         }
-                    }
-                    else if (isBack) {
-                        bgColor = "bg-sarireddarker";
-                        zIndex = "z-10";
-                        transform =
-                            "translate3d(-1.8rem,3rem,0) rotate(4deg) scale(.92)";
-                        if (isMoving && direction === "prev") {
-                            zIndex = "z-40";
-
-                            transform =
-                                "translate3d(5rem,-7rem,0) rotate(-7deg) scale(.88)";
-                        }
-                        else if (animating && direction === "next") {
-                            transform =
-                                "translate3d(-2.5rem,4.6rem,0) rotate(5deg) scale(.9)";
-                        }
-                    }
-
-                    return (
-                        <div
-                            key={index}
-                            className={`
-                                testimonial
-                                absolute
-                                ${bgColor}
-                                ${zIndex}
-                                origin-center
-                                will-change-transform
-                            `}
-                            style={{
-                                transform,
-                                transitionProperty: "transform",
-                                transitionDuration: "900ms",
-                                transitionTimingFunction:
-                                    "cubic-bezier(0.16, 1, 0.3, 1)",
-                            }}
-                        >
-                            <div className="flex flex-col gap-2 w-250 pl-14 py-10">
-                                <img
-                                    src="/src/assets/images/testimonial-user.png"
-                                    alt={item.name}
-                                    className="-mt-25 w-40 h-auto transition-all duration-700"
-                                />
-
-                                <h1 className="text-sarired text-[40px] font-mr font-semibold transition-all duration-700">
-                                    {item.name}
-                                </h1>
-
-                                <h2 className="transition-all duration-700">
-                                    {item.role}
-                                </h2>
+                        return (
+                            <div key={index} className={`testimonial absolute ${bgColor} ${zIndex} origin-center will-change-transform`} style={{transform, transitionProperty: "transform", transitionDuration: "900ms", transitionTimingFunction: "cubic-bezier(0.16, 1, 0.3, 1)",}}>
+                                <div className="flex flex-col gap-2 w-1/2 max-sm:w-full z-2">
+                                    <img src={item.image} alt={item.name} className="-mt-25 w-40 h-auto transition-all duration-700"/>
+                                    <h1 className="text-sarired text-[40px] max-sm:text-3xl font-mr font-semibold transition-all duration-700">
+                                        {item.name}
+                                    </h1>
+                                    <h2 className="transition-all duration-700">
+                                        {item.role}
+                                    </h2>
+                                </div>
+                                <div className="flex flex-col justify-between w-1/2 max-sm:w-full relative overflow-clip rounded-r-[56px] transition-all duration-700 max-sm:gap-6 z-2">
+                                    <p className="text-base/[145%] max-sm:order-2">
+                                        {item.text}
+                                    </p>
+                                    <span className="testimonial-star max-sm:order-1">
+                                        <StarFill className="w-4 h-auto" />
+                                        <StarFill className="w-4 h-auto" />
+                                        <StarFill className="w-4 h-auto" />
+                                        <StarFill className="w-4 h-auto" />
+                                        <StarFill className="w-4 h-auto" />
+                                    </span>
+                                </div>
+                                <div className="absolute top-0 left-0 w-full h-full overflow-hidden testimonial">
+                                    <HealthTrendsRotate className="text-sarired absolute -bottom-17 right-0 h-auto w-42 transition-all duration-700" />
+                                </div>
                             </div>
-
-                            <div className="flex flex-col justify-between pr-14 py-10 relative overflow-clip rounded-r-[56px] transition-all duration-700">
-                                <p className="text-base/[145%]">
-                                    {item.text}
-                                </p>
-
-                                <span className="testimonial-star">
-                                    <StarFill className="w-4 h-auto" />
-                                    <StarFill className="w-4 h-auto" />
-                                    <StarFill className="w-4 h-auto" />
-                                    <StarFill className="w-4 h-auto" />
-                                    <StarFill className="w-4 h-auto" />
-                                </span>
-
-                                <HealthTrendsRotate className="text-sarired absolute -bottom-17 right-0 h-auto w-42 transition-all duration-700" />
-                            </div>
-                        </div>
-                    );
-                })}
+                        );
+                    })}
+                </div>
             </div>
 
             <span className="flex gap-4 mt-6 items-center">
